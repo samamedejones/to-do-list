37f78076c108d79459d7980665743b6d847d075c
Jones <samamedejones@gmail.com>
2024-11-12 23:22:28 -0300

    create tag

---
+++ src/componentes/Tag/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from './styles'
+
+export default function Tag( { title } ) {
+  return (
+    <Container>
+      {title}
+    </Container>
+  )
+}
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,6 +2,7 @@ import { Container, Links } from './style'
 import { Header } from '../../componentes/Header'
 import { Button } from '../../componentes/Button'
 import { Section } from '../../componentes/Section'
+import Tag from '../../componentes/Tag'
 
 export function Details() {
 
@@ -9,13 +10,19 @@ export function Details() {
   return (
     <Container>
       <Header/>
-      <Section tittle="teste">
+      <Section tittle="Lins Uteis">
         <Links>
           <li><a href="#">Link 1</a></li>
           <li><a href="#">Link 2</a></li>
         </Links>
 
       </Section>
+
+      <Section tittle="Marcadores">
+        <Tag title="express"/>
+
+      </Section>
+
       <Button title="Voltar"/>
     </Container>
 )
