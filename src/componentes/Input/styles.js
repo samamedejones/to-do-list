2525f19cc008699ec7da7e81a5ca4844b8214901
Jones <samamedejones@gmail.com>
2024-11-18 23:08:34 -0300

    fonsize/padding

--- src/componentes/Input/styles.js
+++ src/componentes/Input/styles.js
@@ -1,16 +1,6 @@
 import styled from "styled-components"
 
 export const Container = styled.input`
-  grid-area: header;
-
-  height: 105px;
-  width: 100%;
-
-  border-bottom: 1px solid ${({ theme }) => theme.COLORS.BACKGROUND_700};
-
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  padding: 0 80px;
+  font-size: 24px;
+  padding: 10px;
 `
