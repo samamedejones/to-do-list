ab8307db6d2fb6f7e3e8c9ce8668a2e4af6624be
Jones <samamedejones@gmail.com>
2024-11-20 21:47:30 -0300

    styles note

--- src/componentes/Note/index.jsx
+++ src/componentes/Note/index.jsx
@@ -6,6 +6,17 @@ export function Note({ data, ...rest }) {
     return (
         <Container {...rest}>
             <h1>{data.tittle}</h1>
+
+            {
+                data.tags &&
+                <footer>
+                    {
+                        data.tags.map(tags => {
+                            <span></span>
+                        })
+                    }
+                </footer>
+            }
         </Container>
     )
 }
