791a4e79a96b77c08e219abe1dee8b27ce0b1612
Jones <samamedejones@gmail.com>
2024-11-21 23:07:38 -0300

    fs

--- src/componentes/Note/styles.js
+++ src/componentes/Note/styles.js
@@ -2,5 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.button`
     width: 100%;
+    font-size: 24px;
     background-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
 `
