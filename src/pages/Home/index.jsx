f84fb8e2630d4cd2263bdeb4add74c15cb3971f4
Jones <samamedejones@gmail.com>
2024-11-30 23:57:26 -0300

    second note

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -41,8 +41,22 @@ export default function Home() {
                 {id: "4", name: "css"},
                 {id: "5", name: "js note"},
                 {id: "6", name: "html note"},
+                {id: "7", name: "teste"},
                 {id: "7", name: "teste"}
+              ]
+              }}/>
 
+<Note data={{
+              title: "Teste 2", 
+              tags: [
+                {id: "1", name: "react"},
+                {id: "2", name: "rockeatseat"},
+                {id: "3", name: "javascript"},
+                {id: "4", name: "css"},
+                {id: "5", name: "js note"},
+                {id: "6", name: "html note"},
+                {id: "7", name: "teste"},
+                {id: "7", name: "teste"}
               ]
               }}/>
           </Section>
