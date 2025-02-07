36da56abc02e22b4a1c48f4bd20b359a456456e4
Jones <samamedejones@gmail.com>
2025-02-07 20:37:19 -0300

    14localstorage

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,17 +1,20 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import { api } from "../services/api";
 
 export const AuthContext = createContext({})
 
 function AuthProvider({ children }){
+    const [data, setData] = useState({})
 
     async function signIn({ email, password }) {
-        const [data, setData] = useState({})
 
         try {
             const response = await api.post("/sessions", { email, password })
             const { user, token } = response.data
+
+            localStorage.setItem("@rockeatseat:user", JSON.stringify(user))
+            localStorage.setItem("@rockeatseat:token", token)
 
             api.defaults.headers.authorization = `Bearer ${token}`
             setData({ user, token })
@@ -26,6 +29,20 @@ function AuthProvider({ children }){
           }
         }
     }
+
+    useEffect(()=> {
+        const token = localStorage.getItem("@rockeatseat:token")
+        const user = localStorage.getItem("@rockeatseat:user")
+
+        if(token && user){
+            api.defaults.headers.authorization = `Bearer ${token}`
+            setData({
+                token,
+                user: JSON.parse(user)
+            })
+        }
+
+    }, [])
     
     return(
         <AuthContext.Provider value={{ signIn, user: data.user }}>
